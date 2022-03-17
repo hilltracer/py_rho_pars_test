d5a18fce9782e436f9d6c4bfeb2dc23e75351134
hilltracer <hilltracer@yandex.ru>
2022-03-17 23:22:03 +0300

    Add map append test without loop

--- web_console.py
+++ web_console.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 import requests
 
+
 def send_explore_deploy(deploy_data: str):
     return requests.post('http://localhost:40403/api/explore-deploy', data=deploy_data)
 
@@ -32,7 +33,7 @@ if __name__ == '__main__':
                 ans = send_explore_deploy(contract)
                 t2 = datetime.now()
                 t_m = (t2 - t1).seconds + (t2 - t1).microseconds / 1000000
-                print(ans.text)
+                # print(ans.text)
                 print(f"{size},   {t_m}")
 
         elif command.startswith('list'):
@@ -54,8 +55,8 @@ if __name__ == '__main__':
                 ans = send_explore_deploy(contract)
                 t2 = datetime.now()
                 t_m = (t2 - t1).seconds + (t2 - t1).microseconds / 1000000
-                print(ans.text)
-                print(f"{size},   {t_m}")
+                # print(ans.text)
+                print(f"{size}, {t_m}")
 
         elif command.startswith('set'):
             num_of_experiments = 5
@@ -77,15 +78,14 @@ if __name__ == '__main__':
                 ans = send_explore_deploy(contract)
                 t2 = datetime.now()
                 t_m = (t2 - t1).seconds + (t2 - t1).microseconds / 1000000
-                print(ans.text)
-                print(f"{size},   {t_m}")
+                # print(ans.text)
+                print(f"{size}, {t_m}")
 
         elif command.startswith('map'):
-            num_of_experiments = 5
-            step_size = 10
+            num_of_experiments = 10
+            step_size = 100
             for j in range(num_of_experiments):
                 phlo_price, valid = (1, 0)
-                print("experiment number = " + str(j))
                 size = step_size + step_size * j
                 contract = """
                 new return, loop in {
@@ -100,7 +100,31 @@ if __name__ == '__main__':
                 ans = send_explore_deploy(contract)
                 t2 = datetime.now()
                 t_m = (t2 - t1).seconds + (t2 - t1).microseconds / 1000000
-                print(ans.text)
-                print(f"{size},   {t_m}")
+                # print(ans.text)
+                print(f"{size}, {t_m}")
+
+        elif command.startswith('2map'):
+            num_of_experiments = 10
+            step_size = 100
+            for j in range(num_of_experiments):
+                phlo_price, valid = (1, 0)
+                size = step_size + step_size * j
+                map_primitives = "{}"
+                for k in range(1, size+1):
+                    map_primitives = map_primitives + ".set(%s,%s)" % (k, k)
+                contract = """
+                new return, loop in {
+                  contract loop(ret) = {
+                    ret!(%s)
+                  } |
+                  loop!(*return)
+                }
+                """ % map_primitives
+                t1 = datetime.now()
+                ans = send_explore_deploy(contract)
+                t2 = datetime.now()
+                t_m = (t2 - t1).seconds + (t2 - t1).microseconds / 1000000
+                # print(ans.text)
+                print(f"{size}, {t_m}")
 
             break
