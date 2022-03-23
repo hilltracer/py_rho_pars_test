84ba0e92a2f6f836b90aaf2394f802deb47c5469
hilltracer <hilltracer@yandex.ru>
2022-03-23 17:46:56 +0300

    Add another version test of append Map

--- web_console.py
+++ web_console.py
@@ -81,8 +81,8 @@ if __name__ == '__main__':
                 # print(ans.text)
                 print(f"{size}, {t_m}")
 
-        elif command.startswith('map'):
-            num_of_experiments = 10
+        elif command.startswith('mapWithLoop1'):
+            num_of_experiments = 5
             step_size = 100
             for j in range(num_of_experiments):
                 phlo_price, valid = (1, 0)
@@ -103,8 +103,34 @@ if __name__ == '__main__':
                 # print(ans.text)
                 print(f"{size}, {t_m}")
 
-        elif command.startswith('2map'):
-            num_of_experiments = 10
+        elif command.startswith('mapWithLoop2'):
+            num_of_experiments = 5
+            step_size = 100
+            for j in range(num_of_experiments):
+                phlo_price, valid = (1, 0)
+                size = step_size + step_size * j
+                contract = """
+                    @{"loop"}!(%s, {}) |
+                    for( @{x0}, @{x1} <= @{"loop"} ) {
+                      match (x0 == 0) {
+                        true => {
+                          Nil
+                        }
+                        false => {
+                          @{"loop"}!((x0 - 1), (x1).set(x0,x0))
+                        }
+                      }
+                    }
+                 """ % size
+                t1 = datetime.now()
+                ans = send_explore_deploy(contract)
+                t2 = datetime.now()
+                t_m = (t2 - t1).seconds + (t2 - t1).microseconds / 1000000
+                # print(ans.text)
+                print(f"{size}, {t_m}")
+
+        elif command.startswith('mapWithoutLoop'):
+            num_of_experiments = 5
             step_size = 100
             for j in range(num_of_experiments):
                 phlo_price, valid = (1, 0)
@@ -126,5 +152,3 @@ if __name__ == '__main__':
                 t_m = (t2 - t1).seconds + (t2 - t1).microseconds / 1000000
                 # print(ans.text)
                 print(f"{size}, {t_m}")
-
-            break
